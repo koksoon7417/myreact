d810158e0e58738a3acdda9694756cff756c96ca
Kok Soon <koksoon7417@gmail.com>
2019-12-20 15:44:11 +0800

    Updates

---
+++ precache-manifest.e2e0288be620b6ef75a9031cd2471eb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7b156ac36f17f7dfdef991fdf4982b8",
+    "url": "/myreact/index.html"
+  },
+  {
+    "revision": "456ef325090b21074b3b",
+    "url": "/myreact/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a8ddf5ac9ee7aa57165e",
+    "url": "/myreact/static/js/2.cfffadc9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/myreact/static/js/2.cfffadc9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "456ef325090b21074b3b",
+    "url": "/myreact/static/js/main.80a63d61.chunk.js"
+  },
+  {
+    "revision": "dedee0952a1868a43893",
+    "url": "/myreact/static/js/runtime-main.fb57f21d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
